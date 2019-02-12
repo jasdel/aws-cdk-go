923eb2f4aa93caed8d97d87eb4c6e7617309d915
Jason Del Ponte <delpontej@gmail.com>
2019-02-12 13:14:40 -0800

    update extend example with new style

--- examples/extend_cdk_construct/main.go
+++ examples/extend_cdk_construct/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strings"
-
 	"github.com/awslabs/aws-cdk-go/awsapigateway"
 	"github.com/awslabs/aws-cdk-go/awslambda"
 	"github.com/awslabs/aws-cdk-go/cdk"
@@ -11,65 +9,79 @@ import (
 func main() {
 	app := cdk.NewApp()
 
-	addHandlerStack(app, cdk.StackProps_{
-		NamingScheme_: NamingSchemeFunc(func(parts []string) string {
-			return strings.Join(parts, ":")
-		}),
-	})
+	addHandlerStack(app)
 
 	app.Run()
 }
 
-type NamingSchemeFunc func([]string) string
+func addHandlerStack(app cdk.App) {
+	stack := cdk.NewStack(app, "MyHandlerStack", nil)
 
-func (fn NamingSchemeFunc) AlocateAddress(parts []string) string {
-	return fn(parts)
-}
-
-func addHandlerStack(app cdk.App, stackProps cdk.StackProps) {
-	stack := cdk.NewStack(app, "MyHandlerStack", stackProps)
-
-	validator := func(fn awslambda.Function) []string {
-		if fn.FunctionName() != "HelloHandler" {
-			return []string{"Wrong name for handler"}
-		}
-		return nil
-	}
-
-	// Custom Lambda Funtion construct with support for sepcifing a custom
-	// validator that will be used when the function construct is syntisized.
-	helloFn := NewValidateLambdaFunction(stack, "HelloHandler", validator, awslambda.FunctionProps_{
-		Runtime_: awslambda.Runtime_NodeJS810(),  // Execution environment.
-		Code_:    awslambda.Code_Asset("lambda"), // Code loaded from the "lambda" directory.
-		Handler_: "hello.Handler",                // File is "hello", function is "handler.
+	// Custom Lambda Function construct with support for specifying a custom
+	// validator that will be used when the CDK construct is synthesized into a
+	// CloudFormation template.
+	helloFn := NewValidateLambdaFunction(stack, "HelloHandler", ValidateLambdaFunctionProps{
+		FuncProps: awslambda.FunctionProps_{
+			Runtime_: awslambda.Runtime_NodeJS810(),  // Execution environment.
+			Code_:    awslambda.Code_Asset("lambda"), // Code loaded from the "lambda" directory.
+			Handler_: "hello.Handler",                // File is "hello", function is "handler.
+		},
+		// Create a validator function that will be used by ValidateLambdaFunction
+		// when the lambda function CDK construct is generated into A
+		// CloudFormation template
+		Validate: func(fn awslambda.Function) []string {
+			if fn.FunctionName() != "HelloHandler" {
+				return []string{"Wrong name for handler"}
+			}
+			return nil
+		},
 	})
 
 	// Defines an API Gateway REST API resource backed by our "hello" function.
 	awsapigateway.NewLambdaRestApi(stack, "Endpoint", awsapigateway.LambdaRestApiProps_{
 		Handler_: helloFn,
 	})
-
 }
 
+// ValidateLambdaFunctionProps provides property options.
+type ValidateLambdaFunctionProps struct {
+	FuncProps awslambda.FunctionProps
+	Validate  func(awslambda.Function) []string
+}
+
+// ValidateLambdaFunction extends the awslambda.Funtion CDK construct with
+// custom validation that will be used during generation of the CloudFromation
+// template.
 type ValidateLambdaFunction struct {
 	awslambda.Function
 
-	ValidateFn LambdaValidator
+	validateFn func(awslambda.Function) []string
 }
 
-type LambdaValidator func(fn awslambda.Function) []string
-
-func NewValidateLambdaFunction(scope cdk.Construct, id string, validateFn LambdaValidator, props awslambda.FunctionProps) *ValidateLambdaFunction {
+// NewValidateLambdaFunction returns a ValidateLambdaFunction extending the
+// awslambda.Function CDK construct. The custom validator will be used when
+// ValidateLambdaFunction's Validate method is invoked within the CDK runtime.
+func NewValidateLambdaFunction(scope cdk.Construct, id string, props ValidateLambdaFunctionProps) *ValidateLambdaFunction {
 	v := &ValidateLambdaFunction{
-		ValidateFn: validateFn,
+		validateFn: props.Validate,
 	}
 
-	lambda := awslambda.NewFunction_WithOverrides(scope, id, props, v)
+	lambda := awslambda.NewFunction_WithOverrides(scope, id, props.FuncProps, v)
 
 	v.Function = lambda
 	return v
 }
 
+// Validate overrides awslambda.Function's Validate method to perform
+// validation on the lambda function during synthesis of the CloudFromation
+// template.
 func (l *ValidateLambdaFunction) Validate() []string {
-	return l.ValidateFn(l)
+	errs := l.Function.Validate()
+
+	// Use Custom validation if provided
+	if l.validateFn != nil {
+		errs = append(errs, l.validateFn(l.Function)...)
+	}
+
+	return errs
 }
